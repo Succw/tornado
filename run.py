65253635514756b8f08d73fd196bf1fa80404a58
succw <succw@qq.com>
2016-05-24 18:32:27 +0800

    update

--- run.py
+++ run.py
@@ -6,15 +6,15 @@ import tornado.options
 import tornado.httpserver
 
 from tornado.options import define,options
-import os
+import os,random
 
-define('port',default=8081,type=int,help='server port')
+define('port',default=8080,type=int,help='server port')
 
-class IndexHandler(tornado.web.RequestHandler):
+class InfoHandler(tornado.web.RequestHandler):
     def get(self):
-	    self.render('index.html')
+	    self.render('index.html',book='<<&abc>>')
 
-class PostPageHandler(tornado.web.RequestHandler):
+class InfoPostHandler(tornado.web.RequestHandler):
     def post(self):
         name = self.get_argument('name')
         mail = self.get_argument('email')
@@ -25,11 +25,44 @@ class PostPageHandler(tornado.web.RequestHandler):
     def write_error(self, status_code, **kwargs):
         self.write('why? status code: %d' % status_code)
 
+class MungerHandler(tornado.web.RequestHandler):
+    def map_by_first_letter(self,text):
+        mapped = dict()
+        for line in text.split('\r\n'):
+            for word in [x for x in line.split(' ') if len(x) > 0 ]:
+                if word[0] not in mapped:
+                    mapped[word[0]] = []
+                    mapped[word[0]].append(word)
+        return mapped
+
+    def get(self):
+        self.render('munger.html')
+
+    def post(self):
+        source_text = self.get_argument('source')
+        text_to_chage = self.get_argument('change')
+        source_map = self.map_by_first_letter(source_text)
+        change_lines = text_to_chage.split('\r\n')
+        # self.write('source_text: %s, %s ' % (source_text,text_to_chage))
+        self.render('munged.html',source_map=source_map,change_lines=change_lines,choice=random.choice)
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("It's OK!")
+    def post(self):
+        name = self.get_argument('name')
+        self.write('Post Content: %s' % str(name))
+    def write_error(self, status_code, **kwargs):
+        self.write('Error Code: %d' % status_code)
+
 def make_app():
     return tornado.web.Application(handlers=[
-	(r'/',IndexHandler),
-    (r'/post',PostPageHandler)],
-    template_path=os.path.join(os.path.dirname(__file__),"templates")
+    (r'/info',InfoHandler),
+    (r'/infopst',InfoPostHandler),
+    (r'/munger',MungerHandler),
+    (r'/',IndexHandler)],
+    template_path=os.path.join(os.path.dirname(__file__),"templates"),
+    static_path=os.path.join(os.path.dirname(__file__),"static"),
     )
 
 if __name__ == '__main__':
