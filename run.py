d87a184a235ed5772d9e984ccaa6c930706f5369
succw <succw@qq.com>
2016-05-24 14:31:16 +0800

    更新

--- run.py
+++ run.py
@@ -6,17 +6,31 @@ import tornado.options
 import tornado.httpserver
 
 from tornado.options import define,options
+import os
 
 define('port',default=8081,type=int,help='server port')
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-	self.write('succw.com')
+	    self.render('index.html')
+
+class PostPageHandler(tornado.web.RequestHandler):
+    def post(self):
+        name = self.get_argument('name')
+        mail = self.get_argument('email')
+        phone = self.get_argument('phone')
+        addr = self.get_argument('address')
+        self.render('info.html',name=name,mail=mail,phone=phone,address=addr)
+
+    def write_error(self, status_code, **kwargs):
+        self.write('why? status code: %d' % status_code)
 
 def make_app():
     return tornado.web.Application(handlers=[
 	(r'/',IndexHandler),
-])
+    (r'/post',PostPageHandler)],
+    template_path=os.path.join(os.path.dirname(__file__),"templates")
+    )
 
 if __name__ == '__main__':
     tornado.options.parse_command_line()
